11bead34e738e7a09343f78e2e48e059add15b74
appsvelites <apps@velitessport.com>
2026-05-14 12:39:02 +0200

    feat: agregar entrenadores en Pewter City NPC B con diálogos y Pokémon

--- game-src/src/maps/pewter-city-npc-b.ts
+++ game-src/src/maps/pewter-city-npc-b.ts
@@ -1,4 +1,6 @@
+import { beauty, gentleman, jrTrainerMale, youngster } from "../app/npcs";
 import image from "../assets/map/pewter-city-npc-house.png";
+import { Direction } from "../state/state-types";
 import { MapId, MapType } from "./map-types";
 
 const peweterCityNpcB: MapType = {
@@ -29,6 +31,69 @@ const peweterCityNpcB: MapType = {
   },
   exitReturnMap: MapId.PewterCity,
   grass: {},
+  // Trainers para "pewter-city-npc-b"
+trainers: [
+  {
+  npc: gentleman,
+  pokemon: [{ id: 19, level: 2 }],
+  facing: Direction.Down,
+  pos: { x: 4, y: 1 },
+  intro: [
+
+  ],
+  outtro: [
+    "Me pregunto si Diego irá con sombrero a la boda..."
+  ],
+  money: 0,
+  persistent: true,
+},
+  {
+  npc: jrTrainerMale,
+  pokemon: [{ id: 19, level: 2 }],
+  facing: Direction.Up,
+  pos: { x: 7, y: 2 },
+  intro: [
+
+  ],
+  outtro: [
+    "Mike llevará su boina??",
+    "Fijo que a Eva le hace mucha ilusión"
+  ],
+  money: 0,
+  persistent: true,
+},
+  {
+  npc: youngster,
+  pokemon: [{ id: 19, level: 2 }],
+  facing: Direction.Right,
+  pos: { x: 0, y: 3 },
+  intro: [
+
+  ],
+  outtro: [
+    "En si solo he venido para ver la actuación del cuchillos..."
+  ],
+  money: 0,
+  persistent: true,
+},
+  {
+  npc: beauty,
+  pokemon: [{ id: 19, level: 2 }],
+  facing: Direction.Right,
+  pos: { x: 7, y: 5 },
+  intro: [
+
+  ],
+  outtro: [
+    "Aaaah!",
+    "Ufff, menos mal",
+    "Pensaba que eras Fer con otro de sus chistes",
+    "Por favor no le digas que estoy aquí"
+  ],
+  money: 0,
+  persistent: true,
+}
+],
 };
 
 export default peweterCityNpcB;
