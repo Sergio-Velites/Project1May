c91b322206246d8ffe4deeb4f5e1ffa4614a7027
appsvelites <apps@velitessport.com>
2026-05-14 12:32:49 +0200

    feat: agregar entrenador biker con diálogo y objeto bicicleta en Pewter City

--- game-src/src/maps/pewter-city-npc-a.ts
+++ game-src/src/maps/pewter-city-npc-a.ts
@@ -1,4 +1,7 @@
+import { biker } from "../app/npcs";
+import { ItemType } from "../app/use-item-data";
 import image from "../assets/map/pewter-city-npc-house.png";
+import { Direction } from "../state/state-types";
 import { MapId, MapType } from "./map-types";
 
 const peweterCityNpcA: MapType = {
@@ -29,6 +32,33 @@ const peweterCityNpcA: MapType = {
   },
   exitReturnMap: MapId.PewterCity,
   grass: {},
+  // Trainers para "pewter-city-npc-a"
+trainers: [
+  {
+  npc: biker,
+  pokemon: [{ id: 19, level: 2 }],
+  facing: Direction.Left,
+  pos: { x: 5, y: 5 },
+  intro: [
+
+  ],
+  outtro: [
+    "Tadej Pogačar:",
+    "Pensaba que me invitarían a través de Gudari,",
+    "Pero me equivoqué.",
+    "Me sobra una bici.",
+    "Si la quieres es tuya."
+  ],
+  money: 0,
+  persistent: true,
+}
+],
+  items: [
+    {
+      item: ItemType.Bicycle,
+      pos: { x: 7, y: 5 },
+    },
+  ],
 };
 
 export default peweterCityNpcA;
