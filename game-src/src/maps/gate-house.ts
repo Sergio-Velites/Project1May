dd24c9b80da03adbe5b5fac19417114ad8caee14
appsvelites <apps@velitessport.com>
2026-05-23 14:51:25 +0200

    feat: actualizar mapas de gate-house y league-route — optimización de estructuras y adición de encuentros

--- game-src/src/maps/gate-house.ts
+++ game-src/src/maps/gate-house.ts
@@ -8,59 +8,53 @@ const gateHouse: MapType = {
   image,
   height: 8,
   width: 10,
-  start: {
-    x: 4,
-    y: 6,
-  },
+  start: { x: 4, y: 6 },
   walls: {
-    0: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    1: [3, 6],
-    6: [3, 6],
-    7: [3, 6],
-  },
+      0: [0, 1, 2, 3, 6, 7, 8, 9],
+      1: [3, 6],
+      6: [3, 6],
+      7: [3, 6],
+    },
   fences: {},
   grass: {},
-  text: {
-    0: {
-      4: ["La puerta está cerrada."],
-      5: ["La puerta está cerrada."],
-    },
-  },
+  text: {},
   maps: {},
-  exits: {
-    7: [4, 5],
-  },
-  exitReturnPos: {
-    x: 8,
-    y: 6,
-  },
-  exitReturnMap: MapId.Route22,
-  gifts: [
-    {
-      pokemonId: 133,
-      level: 15,
-      pos: { x: 9, y: 2 },
-      questId: "gate-house-gift-9-2",
+  teleports: {
+      0: {
+        4: { map: MapId.LeagueRoute, pos: { x: 8, y: 156 } },
+        5: { map: MapId.LeagueRoute, pos: { x: 9, y: 156 } },
+      },
     },
-  ],
-  // Trainers para "gate-house"
-trainers: [
-  {
-  npc: pokeManiac,
-  pokemon: [{ id: 19, level: 2 }],
-  facing: Direction.Up,
-  pos: { x: 8, y: 2 },
-  intro: [
+  exits: {
+      7: [4, 5],
+    },
+  exitReturnMap: MapId.Route22,
+  exitReturnPos: { x: 8, y: 6 },
+  gifts: [
+      {
+        pokemonId: 133,
+        level: 15,
+        pos: { x: 9, y: 2 },
+        questId: "gate-house-gift-9-2",
+      },
+    ],
+  trainers: [
+    {
+    npc: pokeManiac,
+    pokemon: [{ id: 19, level: 2 }],
+    facing: Direction.Up,
+    pos: { x: 8, y: 2 },
+    intro: [
 
+    ],
+    outtro: [
+      "🎵 Llegaré a ser el mejor",
+      "el mejor que habra jamás🎵..."
+    ],
+    money: 0,
+    persistent: true,
+  }
   ],
-  outtro: [
-    "🎵 Yegare cualquier lugar",
-    "llegaré a cualquier rincón🎵..."
-  ],
-  money: 0,
-  persistent: true,
 }
-],
-};
 
 export default gateHouse;
