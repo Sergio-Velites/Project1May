215f0782028d44b3c5d92b0f25f8c5cb883cd43b
Sergio-Velites <apps@velitessport.com>
2026-06-14 08:38:19 +0200

    editor: actualizar mapa viridian-city

--- game-src/src/maps/viridian-city.ts
+++ game-src/src/maps/viridian-city.ts
@@ -16,43 +16,43 @@ const viridianCity: MapType = {
   width: 40,
   start: { x: 20, y: 34 },
   walls: {
-      0: [16, 20],
-      1: [16, 19, 20, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
-      2: [16, 20, 23, 36],
-      3: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 36],
-      4: [5, 28, 29, 30, 31, 32, 33, 36],
-      5: [5, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31, 32, 33, 36],
-      6: [5, 7, 16, 20, 28, 29, 30, 31, 32, 33, 36],
-      7: [5, 7, 16, 20, 27, 28, 29, 30, 31, 33, 36],
-      8: [5, 7, 16, 20, 21, 22, 23, 36],
-      9: [5, 7, 16, 20, 22, 23, 36],
-      10: [5, 7, 16, 36],
-      11: [5, 7, 16, 36],
-      12: [5, 7, 16, 36],
-      13: [0, 1, 2, 3, 4, 5, 7, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
-      14: [7, 16, 20, 21, 22, 23, 36],
-      15: [7, 16, 20, 22, 23, 36],
-      16: [8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31, 36],
-      17: [17, 20, 21, 22, 23, 28, 29, 30, 31, 36],
-      18: [0, 1, 2, 3, 28, 29, 30, 31, 36],
-      19: [3, 28, 30, 31, 36],
-      20: [3, 36],
-      21: [3, 4, 5, 6, 7, 36],
-      22: [3, 22, 23, 24, 25, 36],
-      23: [3, 8, 9, 22, 23, 24, 25, 36],
-      24: [3, 22, 23, 24, 25, 36],
-      25: [3, 22, 24, 25, 36],
-      26: [3, 36],
-      27: [3, 36],
-      28: [3, 36],
-      29: [3, 21, 36],
-      30: [3, 36],
-      31: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
-      32: [19, 22],
-      33: [19, 22],
-      34: [19, 22],
-      35: [19, 22],
-    },
+    0: [16, 20],
+    1: [16, 19, 20, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
+    2: [16, 20, 23, 36],
+    3: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 36],
+    4: [5, 28, 29, 30, 31, 32, 33, 36],
+    5: [5, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31, 32, 33, 36],
+    6: [5, 7, 16, 20, 28, 29, 30, 31, 32, 33, 36],
+    7: [5, 7, 16, 20, 27, 28, 29, 30, 31, 33, 36],
+    8: [5, 7, 16, 20, 21, 22, 23, 36],
+    9: [5, 7, 16, 20, 22, 23, 36],
+    10: [5, 7, 16, 36],
+    11: [5, 7, 16, 36],
+    12: [5, 7, 16, 36],
+    13: [0, 1, 2, 3, 4, 5, 7, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+    14: [7, 16, 20, 21, 22, 23, 36],
+    15: [7, 16, 20, 22, 23, 36],
+    16: [8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31, 36],
+    17: [17, 20, 21, 22, 23, 28, 29, 30, 31, 36],
+    18: [0, 1, 2, 3, 28, 29, 30, 31, 36],
+    19: [3, 28, 30, 31, 36],
+    20: [3, 36],
+    21: [3, 4, 5, 6, 7, 36],
+    22: [3, 22, 23, 24, 25, 36],
+    23: [3, 8, 9, 22, 23, 24, 25, 36],
+    24: [3, 22, 23, 24, 25, 36],
+    25: [3, 22, 24, 25, 36],
+    26: [3, 36],
+    27: [3, 36],
+    28: [3, 36],
+    29: [3, 21, 36],
+    30: [3, 36],
+    31: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+    32: [19, 22],
+    33: [19, 22],
+    34: [19, 22],
+    35: [19, 22],
+  },
     boulders: [
     {
       pos: { x: 32, y: 14 },
@@ -80,86 +80,86 @@ const viridianCity: MapType = {
     },
   ],
   fences: {
-      9: [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
-      27: [4, 5, 6, 7, 14, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
-    },
+    9: [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
+    27: [4, 5, 6, 7, 14, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
+  },
   grass: {},
   water: {
-      24: [8, 9, 10, 11, 12, 13],
-      25: [8, 9, 10, 11, 12, 13],
-      26: [8, 9, 10, 11, 12, 13],
-      27: [8, 9, 10, 11, 12, 13],
-    },
+    24: [8, 9, 10, 11, 12, 13],
+    25: [8, 9, 10, 11, 12, 13],
+    26: [8, 9, 10, 11, 12, 13],
+    27: [8, 9, 10, 11, 12, 13],
+  },
   text: {
-      1: {
-        19: [
-          "RUTA 2 · Hacia VILLAMAYOR →"
-        ],
-      },
-      7: {
-        27: [
-          "RINCÓN DE PENSAR. Lugar abandonado. Antiguo emplazamiento para quedada de cuadrillas"
-        ],
-      },
-      17: {
-        17: [
-          "AVISO: Reserva de vino protegida.",
-          "Manos fuera, Team Rocket."
-        ],
-      },
-      19: {
-        30: [
-          "¡Que vivan los novios!",
-          "¡Y que el anís no falte!"
-        ],
-      },
-      25: {
-        24: [
-          "En el Soto, el vino corre más que el agua."
-        ],
-      },
-      29: {
-        21: [
-          "¡SOTO LEZKAIRU!",
-          "Bienvenido al glamour."
-        ],
-      },
+    1: {
+      19: [
+        "RUTA 2 · Hacia VILLAMAYOR →"
+      ],
     },
+    7: {
+      27: [
+        "RINCÓN DE PENSAR. Lugar abandonado. Antiguo emplazamiento para quedada de cuadrillas"
+      ],
+    },
+    17: {
+      17: [
+        "AVISO: Reserva de vino protegida.",
+        "Manos fuera, Team Rocket."
+      ],
+    },
+    19: {
+      30: [
+        "¡Que vivan los novios!",
+        "¡Y que el anís no falte!"
+      ],
+    },
+    25: {
+      24: [
+        "En el Soto, el vino corre más que el agua."
+      ],
+    },
+    29: {
+      21: [
+        "¡SOTO LEZKAIRU!",
+        "Bienvenido al glamour."
+      ],
+    },
+  },
   maps: {
-      0: {
-        17: MapId.Route2,
-        18: MapId.Route2,
-        19: MapId.Route2,
-      },
-      7: {
-        32: MapId.ViridianCityGym,
-      },
-      9: {
-        21: MapId.ViridianCityNpcHouse,
-      },
-      14: {
-        0: MapId.Route22,
-      },
-      15: {
-        0: MapId.Route22,
-        21: MapId.ViridianCityPokemonAcadamy,
-      },
-      16: {
-        0: MapId.Route22,
-      },
-      17: {
-        0: MapId.Route22,
-      },
-      19: {
-        29: MapId.ViridianCityPokeMart,
-      },
-      25: {
-        23: MapId.ViridianCityPokemonCenter,
-      },
+    0: {
+      17: MapId.Route2,
+      18: MapId.Route2,
+      19: MapId.Route2,
     },
+    7: {
+      32: MapId.ViridianCityGym,
+    },
+    9: {
+      21: MapId.ViridianCityNpcHouse,
+    },
+    14: {
+      0: MapId.Route22,
+    },
+    15: {
+      0: MapId.Route22,
+      21: MapId.ViridianCityPokemonAcadamy,
+    },
+    16: {
+      0: MapId.Route22,
+    },
+    17: {
+      0: MapId.Route22,
+    },
+    19: {
+      29: MapId.ViridianCityPokeMart,
+    },
+    25: {
+      23: MapId.ViridianCityPokemonCenter,
+    },
+  },
   exits: {
-      35: [20, 21],
-    },
+    35: [20, 21],
+  },
   exitReturnMap: MapId.Route1,
   exitReturnPos: { x: 11, y: 1 },
   recoverLocation: { x: 23, y: 26 },
@@ -215,33 +215,35 @@ const viridianCity: MapType = {
     roughTerrain: { rate: 0, pokemon: [] }, gift: { rate: 0, pokemon: [] }, giftEgg: { rate: 0, pokemon: [] }, onlyOne: { rate: 0, pokemon: [] },
   },
   items: [
-      {
-        item: ItemType.MasterBall,
-        pos: { x: 5, y: 23 },
-      },
-    ],
-  staticPokemon: [
-      {
-        pokemonId: 175,
-        level: 5,
-        sprite: "cute-a",
-        pos: { x: 12, y: 4 },
-        questId: "viridian-city-static-12-4",
-        intro: ["Priiii"],
-      },
-    ],
-  cuttableTrees: [
-      {
-        pos: { x: 7, y: 22 },
-        questId: "cut-tree-viridian-city-7-22",
-      },
-      {
-        pos: { x: 15, y: 4 },
-        questId: "cut-tree-viridian-city-15-4",
-      },
-    ],
-  trainers: [
     {
+      item: ItemType.MasterBall,
+      pos: { x: 5, y: 23 },
+    },
+  ],
+  staticPokemon: [
+    {
+      pokemonId: 175,
+      level: 5,
+      sprite: "cute-a",
+      pos: { x: 12, y: 4 },
+      questId: "viridian-city-static-12-4",
+      intro: [
+        "Priiii",
+      ],
+    },
+  ],
+  cuttableTrees: [
+    {
+      pos: { x: 7, y: 22 },
+      questId: "cut-tree-viridian-city-7-22",
+    },
+    {
+      pos: { x: 15, y: 4 },
+      questId: "cut-tree-viridian-city-15-4",
+    },
+  ],
+  trainers: [
+  {
     npc: cueBall,
     pokemon: [{ id: 19, level: 6 }],
     facing: Direction.Down,
@@ -257,7 +259,7 @@ const viridianCity: MapType = {
     money: 80,
     persistent: true,
   },
-    {
+  {
     npc: jrTrainerFemale,
     pokemon: [{ id: 41, level: 7 }],
     facing: Direction.Right,
@@ -272,7 +274,7 @@ const viridianCity: MapType = {
     money: 100,
     persistent: true,
   },
-    {
+  {
     npc: teamRocketGrunt,
     pokemon: [{ id: 33, level: 7 }, { id: 52, level: 6 }],
     facing: Direction.Down,
@@ -288,14 +290,12 @@ const viridianCity: MapType = {
     persistent: true,
     sightRange: 4,
   },
-    {
+  {
     npc: gentleman,
     pokemon: [{ id: 1, level: 1 }],
     facing: Direction.Down,
     pos: { x: 27, y: 22 },
-    intro: [
-
-    ],
+    intro: [],
     outtro: [
       "Joven, el vino tinto cura... y anima.",
       "Toma una botella para el camino.",
@@ -304,14 +304,12 @@ const viridianCity: MapType = {
     money: 0,
     persistent: true,
   },
-    {
+  {
     npc: rocker,
     pokemon: [{ id: 25, level: 1 }],
     facing: Direction.Right,
     pos: { x: 18, y: 8 },
-    intro: [
-
-    ],
+    intro: [],
     outtro: [
       "¡El equipo de sonido ya está enchufado!",
       "¡En cuanto lleguen los novios, esto se lía!"
@@ -319,14 +317,12 @@ const viridianCity: MapType = {
     money: 0,
     persistent: true,
   },
-    {
+  {
     npc: lass,
     pokemon: [{ id: 35, level: 3 }],
     facing: Direction.Left,
     pos: { x: 9, y: 4 },
-    intro: [
-
-    ],
+    intro: [],
     outtro: [
       "Llegué la primera. Esta silla ya tiene dueña.",
       "¡A ver si venís más tarde!"
@@ -334,14 +330,12 @@ const viridianCity: MapType = {
     money: 0,
     persistent: true,
   },
-    {
+  {
     npc: sailor,
     pokemon: [{ id: 54, level: 5 }],
     facing: Direction.Left,
     pos: { x: 20, y: 21 },
-    intro: [
-
-    ],
+    intro: [],
     outtro: [
       "¡Quieto! ¡Esto es para las fotos del álbum!",
       "¡La luz aquí es perfecta!"
@@ -349,7 +343,7 @@ const viridianCity: MapType = {
     money: 0,
     persistent: true,
   },
-    {
+  {
     npc: fisher,
     pokemon: [{ id: 118, level: 7 }, { id: 129, level: 6 }],
     facing: Direction.Down,
@@ -365,6 +359,7 @@ const viridianCity: MapType = {
     persistent: true,
   }
   ],
+  minimapPos: { x: 52, y: 112 },
 }
 
 export default viridianCity;
