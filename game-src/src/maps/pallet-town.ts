a0f04d7b7600dd4327c9af03f27142e90fb26363
Sergio-Velites <apps@velitessport.com>
2026-06-14 13:05:50 +0200

    editor: actualizar mapa pallet-town

--- game-src/src/maps/pallet-town.ts
+++ game-src/src/maps/pallet-town.ts
@@ -206,7 +206,7 @@ encounters: {
   roughTerrain: { rate: 0, pokemon: [] }, gift: { rate: 0, pokemon: [] }, giftEgg: { rate: 0, pokemon: [] }, onlyOne: { rate: 0, pokemon: [] },
 },
   fences: {},
-  minimapPos: { x: 54, y: 136 },
+  minimapPos: { x: 55, y: 135 },
   flyable: true,
   flySpot: { x: 5, y: 6 },
 };
