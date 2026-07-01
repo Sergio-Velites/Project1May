f22dfaeea9e0cd00ac7c80ab4a4b562662d128ac
Sergio-Velites <apps@velitessport.com>
2026-07-01 20:08:59 +0200

    editor: actualizar mapa pallet-town-house-a-2f

--- game-src/src/maps/pallet-town-house-a-2f.ts
+++ game-src/src/maps/pallet-town-house-a-2f.ts
@@ -6,38 +6,42 @@ const houseA2f: MapType = {
   image: houseA2FImage,
   height: 8,
   width: 8,
-  start: {
-    x: 6,
-    y: 2,
-  },
+  start: { x: 6, y: 2 },
   walls: {
     0: [0, 1, 2, 3, 4, 5, 6, 7],
     1: [0, 1, 2],
     4: [3],
+    5: [3],
     6: [0, 6],
-    7: [0],
+    7: [0, 6],
   },
   text: {
-    5: {
-      3: ["¡Estás jugando al Super Smash Brosh en la Nintendo 64!", "... ¡Vamos! ¡Es hora de salir!"],
-    },
     1: {
-      0: ["Enciendes el PC.", "No funciona..."],
+      0: [
+        "Enciendes el PC.",
+        "No funciona..."
+      ],
+    },
+    5: {
+      3: [
+        "¡Estás jugando al Super Smash Brosh en la Nintendo 64!",
+        "... ¡Vamos! ¡Es hora de salir!"
+      ],
     },
   },
-  maps: {
-    1: { 7: MapId.PalletTownHouseA1F },
-  },
-  exits: {
-    1: [7],
-  },
-  exitReturnPos: {
-    x: 6,
-    y: 2,
-  },
+  maps: {},
+  exits: {},
+  exitReturnPos: { x: 6, y: 2 },
   exitReturnMap: MapId.PalletTownHouseA1F,
   grass: {},
   minimapPos: { x: 53, y: 147 },
+  fences: {},
+  teleports: {
+    1: {
+      7: { map: MapId.PalletTownHouseA1F, pos: { x: 6, y: 2 } },
+    },
+  },
+  trainers: [],
 };
 
 export default houseA2f;
