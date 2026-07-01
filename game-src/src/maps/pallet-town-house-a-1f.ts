3ad773736f2c2dcd2f5c0c5d6b48d3c70eb75583
Sergio-Velites <apps@velitessport.com>
2026-07-01 20:08:36 +0200

    editor: actualizar mapa pallet-town-house-a-1f

--- game-src/src/maps/pallet-town-house-a-1f.ts
+++ game-src/src/maps/pallet-town-house-a-1f.ts
@@ -2,16 +2,12 @@ import houseA1FImage from "../assets/map/pallet-town-house-a-1f.png";
 import { MapId, MapType } from "./map-types";
 import { Direction } from "../state/state-types";
 import { beauty } from "../app/npcs";
-
 const houseA1f: MapType = {
   name: "Casa 1F",
   image: houseA1FImage,
   height: 8,
   width: 8,
-  start: {
-    x: 3,
-    y: 6,
-  },
+  start: { x: 3, y: 6 },
   walls: {
     0: [0, 1, 2, 3, 4, 5, 6, 7],
     1: [0, 1, 3],
@@ -22,37 +18,40 @@ const houseA1f: MapType = {
     1: {
       3: [
         "Hoy es un día muy especial.",
-        "¡El Profesor Oak está esperando!",
+        "¡El Profesor Oak está esperando!"
       ],
     },
   },
-  maps: {
-    1: { 7: MapId.PalletTownHouseA2F },
-  },
-  exits: {
-    7: [2, 3],
-  },
-  exitReturnPos: {
-    x: 5,
-    y: 6,
-  },
+  maps: {},
+  exits: {},
+  exitReturnPos: { x: 5, y: 6 },
   exitReturnMap: MapId.PalletTown,
   grass: {},
   trainers: [
-    {
-      npc: beauty,
-      pokemon: [{ id: 19, level: 1 }],
-      facing: Direction.Up,
-      pos: { x: 6, y: 3 },
-      persistent: true,
-      intro: [],
-      outtro: [
-        "¡Espabila pringao!",
-      ],
-      money: 0,
-    },
+  {
+    npc: beauty,
+    pokemon: [{ id: 19, level: 1 }],
+    facing: Direction.Up,
+    pos: { x: 6, y: 3 },
+    intro: [],
+    outtro: [
+      "¡Espabila pringao!"
+    ],
+    money: 0,
+    persistent: true,
+  }
   ],
   minimapPos: { x: 53, y: 147 },
+  fences: {},
+  teleports: {
+    1: {
+      7: { map: MapId.PalletTownHouseA2F, pos: { x: 7, y: 2 } },
+    },
+    7: {
+      2: { map: MapId.PalletTown, pos: { x: 5, y: 6 } },
+      3: { map: MapId.PalletTown, pos: { x: 5, y: 6 } },
+    },
+  },
 };
 
 export default houseA1f;
