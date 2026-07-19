d5144850ee61a4aa3f8f9d610247aa4a1eae4f02
Sergio-Velites <apps@velitessport.com>
2026-07-19 19:03:04 +0200

    editor: actualizar mapa pewter-city-poke-mart

--- game-src/src/maps/pewter-city-poke-mart.ts
+++ game-src/src/maps/pewter-city-poke-mart.ts
@@ -9,10 +9,7 @@ const pewterCityPokeMart: MapType = {
   image,
   height: 8,
   width: 8,
-  start: {
-    x: 4,
-    y: 6,
-  },
+  start: { x: 4, y: 6 },
   walls: {
     0: [0, 1, 2, 3, 4, 5, 6, 7],
     1: [0, 1, 2, 3, 4, 5, 6, 7],
@@ -22,23 +19,13 @@ const pewterCityPokeMart: MapType = {
     6: [0, 1],
   },
   text: {},
-  maps: {
-
-  },
-  exits: {
-    7: [3, 4],
-  },
-  music,
+  maps: {},
+  exits: {},
+  music: music,
   grass: {},
   exitReturnMap: MapId.PewterCity,
-  exitReturnPos: {
-    x: 23,
-    y: 18,
-  },
-  store: {
-    x: 1,
-    y: 5,
-  },
+  exitReturnPos: { x: 23, y: 18 },
+  store: { x: 1, y: 5 },
   storeItems: [
     ItemType.PokeBall,
     ItemType.GreatBall,
@@ -52,6 +39,14 @@ const pewterCityPokeMart: MapType = {
     ItemType.ThunderStone,
   ],
   minimapPos: { x: 53, y: 52 },
+  fences: {},
+  teleports: {
+    8: {
+      3: { map: MapId.PewterCity, pos: { x: 23, y: 18 } },
+      4: { map: MapId.PewterCity, pos: { x: 23, y: 18 } },
+    },
+  },
+  trainers: [],
 };
 
 export default pewterCityPokeMart;
