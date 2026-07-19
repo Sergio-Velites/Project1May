6a6f9c7791c2f00eb2a64aeda826d8312528b36b
Sergio-Velites <apps@velitessport.com>
2026-07-19 18:13:36 +0200

    editor: actualizar mapa pallet-town-house-b

--- game-src/src/maps/pallet-town-house-b.ts
+++ game-src/src/maps/pallet-town-house-b.ts
@@ -9,48 +9,40 @@ const houseB: MapType = {
   image: houseBImage,
   height: 8,
   width: 8,
-  start: {
-    x: 3,
-    y: 6,
-  },
+  start: { x: 3, y: 6 },
   walls: {
     0: [0, 1, 2, 3, 4, 5, 6, 7],
     1: [0, 1, 7],
-    3: [3, 4],
-    4: [3, 4],
+    2: [-1, 8],
+    3: [-1, 3, 4, 8],
+    4: [-1, 3, 4, 8],
+    5: [-1, 8],
     6: [0, 7],
+    7: [0, 7],
+    8: [1, 4, 5, 6],
   },
   text: {},
-  maps: {
-
-  },
-  exits: {
-    7: [2, 3],
-  },
-  exitReturnPos: {
-    x: 13,
-    y: 6,
-  },
+  maps: {},
+  exits: {},
+  exitReturnPos: { x: 13, y: 6 },
   exitReturnMap: MapId.PalletTown,
   grass: {},
   // Trainers para "pallet-town-house-b"
 trainers: [
   {
-  npc: fisher,
-  pokemon: [{ id: 19, level: 2 }],
-  facing: Direction.Down,
-  pos: { x: 5, y: 5 },
-  intro: [
-
+    npc: fisher,
+    pokemon: [{ id: 19, level: 2 }],
+    facing: Direction.Down,
+    pos: { x: 5, y: 5 },
+    intro: [],
+    outtro: [
+      "El puto Peri y Aos se fueron de ",
+      "pesca sin mi..."
+    ],
+    money: 0,
+    persistent: true,
+  }
   ],
-  outtro: [
-    "El puto Peri y Aos se fueron de ",
-    "pesca sin mi..."
-  ],
-  money: 0,
-  persistent: true,
-}
-],
 items: [
     {
       item: ItemType.OldRod,
@@ -58,6 +50,13 @@ items: [
     },
   ],
   minimapPos: { x: 53, y: 147 },
+  fences: {},
+  teleports: {
+    8: {
+      2: { map: MapId.PalletTown, pos: { x: 13, y: 6 } },
+      3: { map: MapId.PalletTown, pos: { x: 13, y: 6 } },
+    },
+  },
 };
 
 export default houseB;
