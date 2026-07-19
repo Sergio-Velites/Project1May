fa42389bef017c8cc60208ebd628b9e10ded2add
Sergio-Velites <apps@velitessport.com>
2026-07-19 18:14:06 +0200

    editor: actualizar mapa pallet-town-lab

--- game-src/src/maps/pallet-town-lab.ts
+++ game-src/src/maps/pallet-town-lab.ts
@@ -2,7 +2,6 @@ import labImage from "../assets/map/pallet-town-lab.png";
 import { MapId, MapType } from "./map-types";
 import { Direction } from "../state/state-types";
 import { oak } from "../app/npcs";
-
 import music from "../assets/music/maps/oaks-laboratory.mp3";
 
 const lab: MapType = {
@@ -14,55 +13,46 @@ const lab: MapType = {
   start: { x: 5, y: 10 },
   walls: {
     0: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    1: [0, 1, 6, 7, 8, 9],
+    1: [0, 1, 2, 3, 6, 7, 8, 9],
+    2: [-1, 10],
+    3: [-1, 6, 7, 8, 10],
+    4: [-1, 10],
+    5: [-1, 10],
     6: [0, 1, 2, 3, 6, 7, 8, 9],
     7: [0, 1, 2, 3, 6, 7, 8, 9],
+    8: [-1, 10],
+    9: [-1, 10],
+    10: [-1, 10],
+    11: [-1, 10],
+    12: [0, 1, 2, 3, 6, 7, 8, 9, 10],
   },
   fences: {},
   grass: {},
   text: {
-      1: {
-        0: [
-          "Enciendes el PC.",
-          "No funciona..."
-        ],
-        5: [
-          "¡Hip! ¡Hoy estamos todos de boda!",
-          "¡Elige un POKEMON de la mesa... hip!",
-          "¡Y date prisa al BOSQUECILLO!",
-          "¡Yo ya voy de buen borraja!"
-        ],
-      },
+    1: {
+      0: [
+        "Enciendes el PC.",
+        "No funciona...",
+        "Qué castaña"
+      ],
     },
-  maps: {
-
   },
-  exits: {
-      11: [4, 5],
-    },
+  maps: {},
+  exits: {},
   exitReturnMap: MapId.PalletTown,
   exitReturnPos: { x: 12, y: 12 },
   trainers: [
-    {
+  {
     npc: oak,
     pokemon: [{ id: 19, level: 2 }],
     facing: Direction.Down,
     pos: { x: 5, y: 1 },
-    intro: [
-
-    ],
+    intro: [],
     outtro: [
-      "Hoy no hay tiempo de tonterías.",
-      "Pon las pilas que es un día importante.",
-      "Te esperan en EL BOSQUECILLO.",
-      "hip!",
-      "Yo ya voy bien borraja.",
-      "hip!",
-      "Date prisa!",
-      "¡Elige uno de los tres POKEMON de la mesa!",
-      "Y corre a merendar!",
-      "He oido que hay un tal Juanre",
-      "que no perdona a los rezagados."
+      "¡Hip! ¡Hoy estamos todos de boda!",
+      "¡Elige un POKEMON de la mesa... hip!",
+      "¡Y date prisa al BOSQUECILLO!",
+      "¡Yo ya estoy borraja!"
     ],
     money: 0,
     persistent: true,
@@ -70,6 +60,12 @@ const lab: MapType = {
   }
   ],
   minimapPos: { x: 53, y: 147 },
+  teleports: {
+    12: {
+      4: { map: MapId.PalletTown, pos: { x: 12, y: 12 } },
+      5: { map: MapId.PalletTown, pos: { x: 12, y: 12 } },
+    },
+  },
 }
 
 export default lab;
