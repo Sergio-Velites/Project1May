232b9fb374bf0536d35d3759108593519607f1a0
Sergio-Velites <apps@velitessport.com>
2026-08-03 16:03:32 +0200

    editor: actualizar mapa route-10

--- game-src/src/maps/route-10.ts
+++ game-src/src/maps/route-10.ts
@@ -221,6 +221,7 @@ const route10: MapType = {
     47: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
   },
   trainers: [],
+  minimapPos: { x: 197, y: 50 },
 };
 
 export default route10;
